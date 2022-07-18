a98bc6d9c14c30e9c4e8d8cb71693790dbcddb7d
James Clarke <james@clarkezone.net>
2022-07-18 14:04:50 -0700

    WIP golang helloworld

--- testapps/golang/simplehttpserver/main.go
+++ testapps/golang/simplehttpserver/main.go
@@ -1,5 +1,37 @@
+// From https://go.dev/play/ HttpServer
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
 func main() {
-	http
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, playground")
+	})
+
+	log.Println("Starting server...")
+	l, err := net.Listen("tcp", "localhost:8079")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		log.Fatal(http.Serve(l, nil))
+	}()
+
+	log.Println("Sending request...")
+	res, err := http.Get("http://localhost:8079/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Reading response...")
+	if _, err := io.Copy(os.Stdout, res.Body); err != nil {
+		log.Fatal(err)
+	}
 }
